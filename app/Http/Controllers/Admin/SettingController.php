c9592145ae5985fdb9795fb7ce945e9e2e958b62
Getúlio Santos <98064959+GetJose@users.noreply.github.com>
2024-04-30 09:09:03 -0300

    creating method save on SettingController and form to update settingtable

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -8,9 +8,31 @@ use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    public function index(){
-        $data['settings'] = Setting::all();
-        
-        return view('Admin.settings.index', $data);
+    public function index()
+    {
+        $dbsettings = Setting::all();
+        foreach ($dbsettings as $setting) {
+            $settings[$setting['name']] = $setting['content'];
+        }
+
+        return view('Admin.settings.index', ['settings' => $settings]);
+    }
+
+    public function save(Request $request)
+    {
+        $request->validate([
+            'email' => 'email|required',
+            'bgcolor' => 'regex:/#[A-Z0-9]{6}/i',
+            'textcolor' => 'regex:/#[A-Z0-9]{6}/i'
+        ]);
+
+        $data = $request->only(['title', 'subtitle', 'email', 'bgcolor', 'textcolor']);
+       
+        foreach ($data as $item => $value) {
+            Setting::where('name', $item)->update([
+                'content' => $value
+            ]);
+        }
+        return redirect()->route('settings');
     }
 }
--- resources/views/Admin/settings/index.blade.php
+++ resources/views/Admin/settings/index.blade.php
@@ -6,6 +6,19 @@
 @endsection
 
 @section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <h5>
+                <i class="icon fas fa-ban"></i>
+                Ocorreu um erro!
+            </h5>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <div class="card">
         <div class="card-body">
             <table class="table table-hover">
@@ -15,32 +28,36 @@
                     <div class="form-group row">
                         <label for="title" class="col-sm-2 col-form-label ">Titulo do site</label>
                         <div class="col-sm-10">
-                            <input type="text" name="title" class="form-control">
+                            <input type="text" name="title" class="form-control" value="{{ $settings['title'] }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="subtitle" class="col-sm-2 col-form-label ">Sub-titulo</label>
                         <div class="col-sm-10">
-                            <input type="text" name="subtitle" class="form-control">
+                            <input type="text" name="subtitle" class="form-control" value="{{ $settings['subtitle'] }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="email" class="col-sm-2 col-form-label ">E-mail de contato</label>
+                        <label for="email" class="col-sm-2 col-form-label ">E-mail de
+                            contato</label>
                         <div class="col-sm-10">
-                            <input type="email" name="email" class="form-control">
+                            <input type="email" name="email" class="form-control" value="{{ $settings['email'] }}">
                         </div>
                     </div>
 
                     <div class="form-group row">
-                        <label for="bgcolor" class="col-sm-2 col-form-label ">Cor de fundo</label>
+                        <label for="bgcolor" class="col-sm-2 col-form-label ">Cor de
+                            fundo</label>
                         <div class="col-sm-10">
-                            <input type="color" name="bgcolor" class="form-control">
+                            <input type="color" name="bgcolor" class="form-control" value="{{ $settings['bgcolor'] }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="textcolor" class="col-sm-2 col-form-label ">Cor da letra</label>
+                        <label for="textcolor" class="col-sm-2 col-form-label ">Cor da
+                            letra</label>
                         <div class="col-sm-10">
-                            <input type="color" name="textcolor" class="form-control">
+                            <input type="color" name="textcolor" class="form-control"
+                                value="{{ $settings['textcolor'] }}">
                         </div>
                     </div>
                     <div class="form-group row">
